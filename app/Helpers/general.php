7243a7f15a19c58fc435f7066b2b18ac6c3ba42c
John Chishugi <jkchishugi@gmail.com>
2021-11-20 18:15:09 +0100

    ft-add-call-interface-server

--- app/Helpers/general.php
+++ app/Helpers/general.php
@@ -127,3 +127,28 @@ function getAllServers()
 
     return  collect(json_decode($data, true));
 }
+
+function getAllServerInterface($id)
+{
+    //setup the request, you can also use CURLOPT_URL
+    $ch = curl_init('http://172.16.60.22:8774/v2.1/servers/' .$id. '/os-interface');
+
+    // Returns the data/output as a string instead of raw data
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    //Set your auth headers
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'X-Auth-Token:' . env('TOKEN')
+    ));
+
+    // get stringified data/output. See CURLOPT_RETURNTRANSFER
+    $data = curl_exec($ch);
+
+    // get info about the request
+    $info = curl_getinfo($ch);
+    // close curl resource to free up system resources
+    curl_close($ch);
+
+    return  collect(json_decode($data, true));
+}
