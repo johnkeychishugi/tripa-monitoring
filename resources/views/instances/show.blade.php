fb725639216c608eeed43b30aefa4f04d5fa6252
John Chishugi <jkchishugi@gmail.com>
2021-11-21 16:40:34 +0100

    ft-show-dianostic-instance

--- resources/views/instances/show.blade.php
+++ resources/views/instances/show.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
+@extends('layouts.app')
+@php
+$interfaces = $interfaces['interfaceAttachments'];
 
-<body>
-    @php
-        $interfaces = $interfaces['interfaceAttachments'];
+@endphp
 
-    @endphp
+@section('content')
+<div id="page-wrapper">
     <div class="container">
         <h2>INTERFACE #{{ count($interfaces) }}</h2>
         <table class="table">
@@ -28,23 +20,21 @@
             </thead>
             <tbody>
                 @foreach ($interfaces as $item)
-                    <tr>
-                        <td>{{ $item['net_id'] }}</td>
-                        <td>{{ $item['port_id'] }}</td>
-                        <td>{{ $item['mac_addr'] }}</td>
-                        <td>{{ $item['port_state'] }}</td>
-                        <td>
-                            <ul>
-                                <li>Subnet: {{ $item['fixed_ips'][0]['subnet_id'] }}</li>
-                                <li>IP Address: {{ $item['fixed_ips'][0]['ip_address'] }}</li>
-                            </ul>
-                        </td>
-                    </tr>
+                <tr>
+                    <td>{{ $item['net_id'] }}</td>
+                    <td>{{ $item['port_id'] }}</td>
+                    <td>{{ $item['mac_addr'] }}</td>
+                    <td>{{ $item['port_state'] }}</td>
+                    <td>
+                        <ul>
+                            <li>Subnet: {{ $item['fixed_ips'][0]['subnet_id'] }}</li>
+                            <li>IP Address: {{ $item['fixed_ips'][0]['ip_address'] }}</li>
+                        </ul>
+                    </td>
+                </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-
-</body>
-
-</html>
+</div>
+@endsection
