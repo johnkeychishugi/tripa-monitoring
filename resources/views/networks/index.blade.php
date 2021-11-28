39c7f147e6281ea63876b49829f55779e621cd22
John Chishugi <jkchishugi@gmail.com>
2021-11-28 17:56:41 +0100

    ft-finalization

--- resources/views/networks/index.blade.php
+++ resources/views/networks/index.blade.php
@@ -2,15 +2,20 @@
 
 @php
 
-$networks = $networks['networks'];
-$subnets = $subnets['subnets'];
-$ports = $ports['ports'];
+$networksRegionOne = $networksRegionOne['networks'];
+$subnetsRegionOne = $subnetsRegionOne['subnets'];
+$portsRegionOne = $portsRegionOne['ports'];
+
+$networksRegionTwo = $networksRegionTwo['networks'];
+$subnetsRegionTwo = $subnetsRegionTwo['subnets'];
+$portsRegionTwo = $portsRegionTwo['ports'];
 
 @endphp
 
 @section('content')
 <div id="page-wrapper">
     <div class="container">
+        <h2>REGION 1</h2>
         <h2>LIST NETWORKS</h2>
         <table class="table">
             <thead>
@@ -27,7 +32,7 @@ $ports = $ports['ports'];
                 </tr>
             </thead>
             <tbody>
-                @foreach ($networks as $key => $item)
+                @foreach ($networksRegionOne as $key => $item)
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $item['id'] }}</td>
@@ -59,7 +64,7 @@ $ports = $ports['ports'];
                 </tr>
             </thead>
             <tbody>
-                @foreach ($subnets as $key => $item)
+                @foreach ($subnetsRegionOne as $key => $item)
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $item['id'] }}</td>
@@ -96,7 +101,7 @@ $ports = $ports['ports'];
                 </tr>
             </thead>
             <tbody>
-                @foreach ($ports as $key => $item)
+                @foreach ($portsRegionOne as $key => $item)
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $item['id'] }}</td>
@@ -116,6 +121,114 @@ $ports = $ports['ports'];
                 @endforeach
             </tbody>
         </table>
+        <br>
+        {{-- Region 2 --}}
+        <hr class="separate-bar">
+        <h2>REGION 2</h2>
+        <h2>LIST NETWORKS</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>ID Network</th>
+                    <th>Name</th>
+                    <th>Tenant ID</th>
+                    <th>MTU</th>
+                    <th>Status</th>
+                    <th>Shared</th>
+                    <th>Availability Zone</th>
+                    <th>Date of Creation</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($networksRegionTwo as $key => $item)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $item['id'] }}</td>
+                    <td>{{ $item['name'] }}</td>
+                    <td>{{ $item['tenant_id'] }}</td>
+                    <td>{{ strval($item['mtu']) }}</td>
+                    <td>{{ $item['status'] }}</td>
+                    <td>{{ $item['shared'] }}</td>
+                    <td>{{ $item['availability_zones'] ? $item['availability_zones'][0] : '--'  }}</td>
+                    <td>{{ $item['created_at'] }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <br>
+        <h2>LIST SUBNETS</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>ID Subnet</th>
+                    <th>Name</th>
+                    <th>ID Network</th>
+                    <th>IP Version</th>
+                    <th>Address</th>
+                    <th>Allocation Pool</th>
+                    <th>DNS Server</th>
+                    <th>Date of Creation</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($subnetsRegionTwo as $key => $item)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $item['id'] }}</td>
+                    <td>{{ $item['name'] }}</td>
+                    <td>{{ $item['network_id'] }}</td>
+                    <td>{{ $item['ip_version'] }}</td>
+                    <td>{{ $item['cidr'] }}</td>
+                    <td>
+                        <ul>
+                            <li>Start: {{ $item['allocation_pools'][0]['start'] }}</li>
+                            <li>End: {{ $item['allocation_pools'][0]['end'] }}</li>
+                        </ul>
+                    </td>
+                    <td>{{ $item['dns_nameservers'] ? $item['dns_nameservers'][0] : '' }}</td>
+                    <td>{{ $item['created_at'] }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <br>
+        <h2>LIST PORTS</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>ID Port</th>
+                    <th>ID Network</th>
+                    <th>MAC Address</th>
+                    <th>Status</th>
+                    <th>Owner</th>
+                    <th>Fixe IP</th>
+                    <th>Date of Creation</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($portsRegionTwo as $key => $item)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $item['id'] }}</td>
+                    <td>{{ $item['network_id'] }}</td>
+                    <td>{{ $item['mac_address'] }}</td>
+                    <td>{{ $item['status'] }}</td>
+                    <td>{{ $item['device_owner'] }}</td>
+                    <td>
+                        <ul>
+                            <li>Subnet ID: {{ $item['fixed_ips'] ? $item['fixed_ips'][0]['subnet_id'] : '--' }}</li>
+                            <li>IP Address: {{ $item['fixed_ips'] ? $item['fixed_ips'][0]['ip_address'] : '--'  }}</li>
+                        </ul>
+                    </td>
+                    <td>{{ $item['created_at'] }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+         
     </div>
 </div>
 @endsection
