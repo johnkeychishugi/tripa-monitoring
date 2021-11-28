a520b544673ffa5264c6b1c385a4bdc65b9994cb
John Chishugi <jkchishugi@gmail.com>
2021-11-28 17:56:37 +0100

    ft-finalization

--- resources/views/instances/index.blade.php
+++ resources/views/instances/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @php
-$servers = $servers['servers'];
+$serversRegionOne = $serversRegionOne['servers'];
+$serversRegionTwo = $serversRegionTwo['servers'];
 
 @endphp
 
 @section('content')
 <div id="page-wrapper">
     <div class="container">
+        <h2>REGION 1</h2>
         <h2>LIST INSTANCES</h2>
         <table class="table">
             <thead>
@@ -26,7 +28,7 @@ $servers = $servers['servers'];
                 </tr>
             </thead>
             <tbody>
-                @foreach ($servers as $key => $item)
+                @foreach ($serversRegionOne as $key => $item)
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $item['id'] }}</td>
@@ -51,7 +53,51 @@ $servers = $servers['servers'];
                     <td>{{ $item['created'] }}</td>
                     <td>
                         <a type="button" class="btn btn-primary"
-                            href="{{ route('server.show',$item['id']) }}">Details</a>
+                            href="{{ route('server.show-region-one',$item['id']) }}">Details</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+         <br>
+         {{-- Region 2 --}}
+         <hr class="separate-bar">
+        <h2>REGION 2</h2>
+        <h2>LIST INSTANCES</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>ID INSTANCE</th>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Image</th>
+                    <th>Flavor</th>
+                    <th>Availability zone</th>
+                    <th>Security Group</th>
+                    <th>Date of Creation</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($serversRegionTwo as $key => $item)
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $item['id'] }}</td>
+                    <td>{{ $item['name'] }}</td>
+                    <td>{{ $item['status'] }}</td>
+                    <td>{{ $item['image']['id'] }}</td>
+                    <td>{{ $item['flavor']['id'] }}</td>
+                    <td>{{ $item['OS-EXT-AZ:availability_zone'] }}</td>
+                    <td>
+                        <ul>
+                            <li>SSH : {{ $item['security_groups'][0]['name'] }}</li>
+                        </ul>
+                    </td>
+                    <td>{{ $item['created'] }}</td>
+                    <td>
+                        <a type="button" class="btn btn-primary"
+                            href="{{ route('server.show-region-two',$item['id']) }}">Details</a>
                     </td>
                 </tr>
                 @endforeach
